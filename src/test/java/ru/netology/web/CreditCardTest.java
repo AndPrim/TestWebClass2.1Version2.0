792bfd5139961bd06b283c7c21a15d757d06704e
AndPrim <andreyminoss@gmail.com>
2024-01-04 04:47:38 +0500

    CreditCardPozitivTwo

--- src/test/java/ru/netology/web/CreditCardTest.java
+++ src/test/java/ru/netology/web/CreditCardTest.java
@@ -3,6 +3,7 @@ package ru.netology.web;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,11 +13,12 @@ public class CreditCardTest {
     public void openShouidTest() throws InterruptedException {
 //        System.setProperty("selenide.headless", "true");
         open("http://localhost:9999/");
-        SelenideElement form = $(".form");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Иванов Иван");
         form.$("[data-test-id=phone] input").setValue("+79998887766");
         form.$("[data-test-id=agreement]").click();
         form.$("button.button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
         Thread.sleep(10000);
     }
 
